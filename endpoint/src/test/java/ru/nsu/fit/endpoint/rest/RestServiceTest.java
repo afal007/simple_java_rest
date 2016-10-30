27e17c446fe2c7b87bdccf87b6144a188ee9c94e
KostblLb <kostya19494@gmail.com>
2016-10-30 19:29:27 +0700

    added MAVEN dependencies

--- endpoint/src/test/java/ru/nsu/fit/endpoint/rest/RestServiceTest.java
+++ endpoint/src/test/java/ru/nsu/fit/endpoint/rest/RestServiceTest.java
@@ -1,5 +1,23 @@
 package ru.nsu.fit.endpoint.rest;
 
-public class RestServiceTest {
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
+public class RestServiceTest {
+ 
+ @BeforeClass
+ public void setUp() {
+   // code that will be invoked when this test is instantiated
+ }
+ 
+ @Test(groups = { "fast" })
+ public void aFastTest() {
+   System.out.println("Fast test");
+ }
+ 
+ @Test(groups = { "slow" })
+ public void aSlowTest() {
+    System.out.println("Slow test");
+ }
+ 
 }
