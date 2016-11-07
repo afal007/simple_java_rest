0028e4992a9ea756720941e6ade96fb80aa7e71f
KostblLb <kostya19494@gmail.com>
2016-11-08 01:34:07 +0700

    doing DBService tests

--- endpoint/src/test/java/ru/nsu/fit/endpoint/service/database/DBServiceTest.java
+++ endpoint/src/test/java/ru/nsu/fit/endpoint/service/database/DBServiceTest.java
@@ -1,20 +1,61 @@
 package ru.nsu.fit.endpoint.service.database;
 
+import java.util.UUID;
+
+import org.testng.Assert;
 import org.testng.annotations.*;
+
+import io.codearte.jfairy.Fairy;
+import io.codearte.jfairy.producer.person.Person;
+import ru.nsu.fit.endpoint.service.database.data.Customer;
+import ru.nsu.fit.endpoint.service.database.data.Plan;
+import ru.nsu.fit.endpoint.service.database.data.Subscription;
+import ru.nsu.fit.endpoint.service.database.data.User;
+import ru.nsu.fit.endpoint.service.database.exceptions.BadCustomerException;
+import ru.nsu.fit.endpoint.service.database.exceptions.BadUserException;
 
 public class DBServiceTest {
  
+ private User testUser;
+ private Customer testCustomer;
+ private Plan testPlan;
+ private Subscription testSubscription;
+ 
+ private Fairy fairy = Fairy.create();
+	 
  @BeforeClass
  public void setUp() {
-   // code that will be invoked when this test is instantiated
+	Person personUser = fairy.person();
+	Person personCustomer = fairy.person();
+	try{
+	testUser = new User(new User.UserData(personUser.firstName(),
+					personUser.lastName(),
+					personUser.email(), 
+					"321StrPass", 
+					User.UserData.UserRole.USER), UUID.randomUUID(), UUID.randomUUID());
+	
+	testCustomer = new Customer(new Customer.CustomerData(personCustomer.firstName(),
+					personCustomer.lastName(), 
+					personCustomer.email(), 
+					"321StrPass", 
+					100), UUID.randomUUID());
+	}
+	catch(BadUserException ex){
+		System.err.println("Test user not cool");
+	}
+	catch(BadCustomerException ex){
+		System.err.println("Test customer not cool");
+	}
  }
  
- @Test(groups = { "fast" })
- public void aFastTest() {
-   System.out.println("Fast test");
+
+ 
+ @Test(expectedExceptions = IllegalArgumentException.class)
+ public void getUserIdNonExistent() {
+	 DBService.getUserById((new UUID(0L, 0L)).toString());
  }
  
- @Test(groups = { "slow" })
+ @Test
  public void aSlowTest() {
     System.out.println("Slow test");
  }
