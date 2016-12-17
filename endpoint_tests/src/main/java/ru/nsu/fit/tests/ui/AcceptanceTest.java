7aa1ca6c20b16bcd1b2014ba7ac0212ba07fc18a
HO-ostage <falalexandr007@gmail.com>
2016-12-17 18:30:47 +0700

    Minor fixes

--- endpoint_tests/src/main/java/ru/nsu/fit/tests/ui/AcceptanceTest.java
+++ endpoint_tests/src/main/java/ru/nsu/fit/tests/ui/AcceptanceTest.java
@@ -1,6 +1,8 @@
 package ru.nsu.fit.tests.ui;
 
 import org.openqa.selenium.By;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -16,11 +18,13 @@ import ru.yandex.qatools.allure.model.SeverityLevel;
  * @author Timur Zolotuhin (tzolotuhin@gmail.com)
  */
 public class AcceptanceTest {
+    private static final Logger logger = LoggerFactory.getLogger("UI_TEST_LOGGER");
     private Browser browser = null;
 
     @BeforeClass
     public void beforeClass() {
         browser = BrowserService.openNewBrowser();
+        org.apache.log4j.BasicConfigurator.configure();
     }
 
     @Test
@@ -30,23 +34,37 @@ public class AcceptanceTest {
     @Features("Customer feature")
     public void createCustomer() {
         // login to admin cp
+        logger.debug("Create customer test");
+
         browser.openPage("http://localhost:8080/endpoint");
         browser.waitForElement(By.id("email"));
+
+        logger.debug("Page is loaded");
 
         browser.getElement(By.id("email")).sendKeys("admin");
         browser.getElement(By.id("password")).sendKeys("setup");
 
+        logger.debug("Filled fields");
+
         browser.getElement(By.id("login")).click();
+
+        logger.debug("Clicked \"Login\" button");
 
         // create customer
         browser.getElement(By.id("add_new_customer")).click();
+
+        logger.debug("Clicked \"Add user\" button");
 
         browser.getElement(By.id("first_name_id")).sendKeys("John");
         browser.getElement(By.id("last_name_id")).sendKeys("Weak");
         browser.getElement(By.id("email_id")).sendKeys("email@example.com");
         browser.getElement(By.id("password_id")).sendKeys("strongpass");
 
+        logger.debug("Filled fields");
+
         browser.getElement(By.id("create_customer_id")).click();
+
+        logger.debug("Clicked \"Create customer\" button");
     }
 
     @Test(dependsOnMethods = "createCustomer")
